01d851092b0c61f05d016faa089ee96234758674
nicolaGuerrieri <guerrierinicola@gmail.com>
2017-01-25 12:34:30 +0100

    chiavi social

--- conf.js
+++ conf.js
@@ -2,12 +2,12 @@ var ids = {
 	facebook : {
 		clientID : "1056996021096310",
 		clientSecret : "ae4d5d4d4a0ee1f65743c7c92ca34f69",
-		callbackURL : 'http://app.nicolaguerrieri.it:3000/successoFacebook/",
+		callbackURL : 'http://app.nicolaguerrieri.it:3000/successoFacebook/',
 	},
 	twitter : {
 		consumerKey : "Ny00XEBokEVSpWaSkgwZjMF8v",
 		consumerSecret : "U4xJjJ4gmyQMNQJIzXBy5HMEvzqsGjvIdUBE4nNRsrZqzLrfHT",
-		callbackURL : 'http://app.nicolaguerrieri.it:3000/successoTwitter/"
+		callbackURL : 'http://app.nicolaguerrieri.it:3000/successoTwitter/'
 	},
 	linkedin : {
 		clientID : "77gmpicgy3yaip",
@@ -22,7 +22,7 @@ var ids = {
 	instagram : {
 		clientID : "2aa696c1848f480c973e0116d4b5cf53",
 		clientSecret : " f0307a22c1a9477e8609df955a0ba452 ",
-		callbackURL : 'http://app.nicolaguerrieri.it:3000/successoInstagram/"
+		callbackURL : 'http://app.nicolaguerrieri.it:3000/successoInstagram/'
 	}
 };
 
